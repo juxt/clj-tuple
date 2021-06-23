220b0b31e7d426ae63833c6d2aee13bf867e7774
James Henderson <james@jarohen.me.uk>
2021-06-23 17:23:47 +0100

    updating project.clj for Clojars mirror

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
-(defproject clj-tuple "0.2.2"
-  :description "Efficient small collections."
+(defproject pro.juxt.clojars-mirrors.ztellman/clj-tuple "0.2.2"
+  :description "Efficient small collections (mirrored from Clojars by JUXT)"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
+  :url "https://github.com/ztellman/clj-tuple"
+  :scm {:name "git"
+        :url "https://github.com/juxt/clj-tuple"
+        :dir "."}
   :dependencies []
+
   :profiles {:dev {:dependencies [[criterium "0.4.1"]
                                   [collection-check "0.1.5"]]}
              :provided {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}}
@@ -12,4 +17,22 @@
                    :default #(every? (complement #{:benchmark :stress}) (keys %))}
   :java-source-paths ["src"]
   :javac-options ["-target" "1.5" "-source" "1.5"]
-  :jvm-opts ^:replace ["-server" "-Xmx500m" "-XX:NewSize=200m"])
+  :jvm-opts ^:replace ["-server" "-Xmx500m" "-XX:NewSize=200m"]
+
+  ;; empty JARs to keep Maven Central happy
+  :classifiers {:sources {:jar-exclusions [#""]}
+                :javadoc {:jar-exclusions [#""]}}
+
+  :pom-addition ([:developers
+                  [:developer
+                   [:id "ztellman"]
+                   [:name "Zach Tellman"]]
+                  [:developer
+                   [:id "juxt"]
+                   [:name "JUXT"]
+                   [:roles [:role "packager"]]]])
+
+  :deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+                                    :creds :gpg}
+                        "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots"
+                                     :creds :gpg}})
